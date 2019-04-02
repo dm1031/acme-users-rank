f86ba36031b07ab84be2ceea6da5becabbb1c1f2
Sam Wellander <samwellander@gmail.com>
2019-04-02 15:57:50 -0400

    add notes

--- server.js
+++ server.js
@@ -1,49 +1,72 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const bodyParser = require('body-parser');
-const { User, syncAndSeed } = require('./db')
+const path = require("path");
+const bodyParser = require("body-parser");
+const { User, syncAndSeed } = require("./db");
 
 const port = process.env.PORT || 3000;
 
-app.get('/app.js', (req, res, next) => res.sendFile(path.join(__dirname, 'dist', 'main.js')));
-app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
+//NOTE: the two routes below, '/app.js' and '/' both serve up static files (files that don't change/aren't dynamic). It's very common in only slightly larger projects to have a lot of static files (images, css style sheets, etc.) When this is the case, it quickly becomes cumbersome to write a unique route for each individual resource. A common solution is to put all static files in the same folder, usually named 'public'. Then express has a handy middleware function, express.static, that we can use to efficiently serve all static files at once.
+
+//Usage would looks something like this:
+
+//*after creating a folder 'public' in the root directory with the below structure
+//public
+// ├───app.js
+// ├───index.html
+
+// app.use(express.static(__dirname + 'public'))
+
+//and that's it!
+
+//more info on express.static: https://expressjs.com/en/starter/static-files.html
+//more info on static serving in general: https://stackoverflow.com/questions/18900990/express-js-node-js-how-does-static-file-serving-really-work
+
+app.get("/app.js", (req, res, next) =>
+  res.sendFile(path.join(__dirname, "dist", "main.js"))
+);
+app.get("/", (req, res, next) =>
+  res.sendFile(path.join(__dirname, "index.html"))
+);
 
 app.use(bodyParser.json());
 
-syncAndSeed()
+//NOTE: syncAndSeed is async and returns a promise so, theoretically, after initially starting the server, it could get hit with requests that try to access the db before the database has finished syncing. There are a number of ways around this. A simple solution would be to chain a .then onto the syncAndSeed call and then run app.listen() within the callback.
 
-app.get('/api/users', (req, res, next) => {
-    User.findAll()
-        .then(users => res.json(users))
-        .catch(next)
-})
+//While this is a very unlikely edge case, it's definitely worth getting used to programming/thinking more defensively.
+syncAndSeed();
 
-app.post('/api/users/create', (req, res, next) => {
-    User.create({
-        name: req.body.name,
-        bio: req.body.bio,
-        rank: req.body.rank
-    })
+app.get("/api/users", (req, res, next) => {
+  User.findAll()
+    .then(users => res.json(users))
+    .catch(next);
+});
+
+app.post("/api/users/create", (req, res, next) => {
+  User.create({
+    name: req.body.name,
+    bio: req.body.bio,
+    rank: req.body.rank
+  })
     .then(user => res.send(user))
-    .catch(next)
-})
+    .catch(next);
+});
 
-app.delete('/api/users/:uuid', (req, res, next) => {
-    User.destroy({
-        where: {
-            uuid: req.params.uuid
-        }
-    })
-    .then( () => res.sendStatus(204))
-    .catch(next)
-})
+app.delete("/api/users/:uuid", (req, res, next) => {
+  User.destroy({
+    where: {
+      uuid: req.params.uuid
+    }
+  })
+    .then(() => res.sendStatus(204))
+    .catch(next);
+});
 
-app.put('/api/users/:uuid', (req, res, next) => {
-    User.findByPk(req.params.uuid)
-        .then(user => user.update(req.body))
-        .then(user => res.send(user))
-        .catch(next)
-})
+app.put("/api/users/:uuid", (req, res, next) => {
+  User.findByPk(req.params.uuid)
+    .then(user => user.update(req.body))
+    .then(user => res.send(user))
+    .catch(next);
+});
 
-app.listen(port, () => console.log(`listening on port ${port}`))
+app.listen(port, () => console.log(`listening on port ${port}`));
--- src/store.js
+++ src/store.js
@@ -1,49 +1,62 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk';
-import axios from 'axios';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import axios from "axios";
 
-const SET_USERS = 'SET_USERS';
+const SET_USERS = "SET_USERS";
 
 const reducer = (state = [], action) => {
-    switch (action.type) {
-        case SET_USERS:
-            state = action.users
-            break;
-        }
-    return state;
-}
+  switch (action.type) {
+    case SET_USERS:
+      //NOTE: here instead of reasigning the new state to the same "state" variable, it's considered best practice to return an entirely new obj as state.
+      //in this particular case, state is just a single array of objects so best practice would look like the following:
 
-const setUsers = (users) => {
-    return {
-        type: SET_USERS,
-        users
-    }
-}
+      //return action.users;
+
+      //reasigning the state variable to the new state (state = action.users) is considered mutation, because it is reusing the same variable (location in memory). one of the main tenents of redux is that it is ideally made up of pure, non-mutating functions. so while your code below works and is completely valid, redux best-practice would be to return a the new state directly without reasigning the state variable.
+      //here's a decent post on this topic: https://stackoverflow.com/questions/37531909/redux-why-is-avoiding-mutations-such-a-fundamental-part-of-using-it
+
+      state = action.users;
+      break;
+  }
+  return state;
+};
+
+const setUsers = users => {
+  return {
+    type: SET_USERS,
+    users
+  };
+};
 
 const fetchUsers = () => {
-    return (dispatch) => {
-        return axios.get('/api/users')
-            .then(response => response.data)
-            .then(users => dispatch(setUsers(users)))
-            .catch(ex => console.log(ex.message))
-    }
-}
+  return dispatch => {
+    return axios
+      .get("/api/users")
+      .then(response => response.data)
+      .then(users => dispatch(setUsers(users)))
+      .catch(ex => console.log(ex.message));
+  };
+};
 
-const onSaveUser = (user) => {
-    return (dispatch) => {
-        return axios[user.uuid ? 'put' : 'post'](`/api/users/${user.uuid ? user.uuid : 'create'}`, user)
-            .then(() => dispatch(fetchUsers()))
-            .catch(ex => console.log(ex.message))
-    }
-}
+const onSaveUser = user => {
+  return dispatch => {
+    return axios[user.uuid ? "put" : "post"](
+      `/api/users/${user.uuid ? user.uuid : "create"}`,
+      user
+    )
+      .then(() => dispatch(fetchUsers()))
+      .catch(ex => console.log(ex.message));
+  };
+};
 
-const destroyUser = (uuid) => {
-    return (dispatch) => {
-        return axios.delete(`/api/users/${uuid}`)
-            .then( () => dispatch(fetchUsers()))
-    }
-}
+const destroyUser = uuid => {
+  return dispatch => {
+    return axios
+      .delete(`/api/users/${uuid}`)
+      .then(() => dispatch(fetchUsers()));
+  };
+};
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const store = createStore(reducer, applyMiddleware(thunk));
 export default store;
-export  { setUsers, fetchUsers, onSaveUser, destroyUser }
+export { setUsers, fetchUsers, onSaveUser, destroyUser };
